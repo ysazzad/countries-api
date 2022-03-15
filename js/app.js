e419cc4052812cfa224fc198888ef848b18f6096
sazzad <hossensazzad1201@gmail.com>
2022-03-15 23:14:29 +0600

    single country added

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const loadCountries = () => {
 const displayCountries = (countries) => {
     // console.log(countries);
     const allCountries = countries.map(country => getCountry(country))
-    console.log(allCountries);
+    // console.log(allCountries);
     const showCountries = document.getElementById("showCountries")
     showCountries.innerHTML = allCountries.join(" ")
 }
@@ -16,8 +16,31 @@ const getCountry = ({ name, flags }) => {
     return `
     <div class= "country">
       <h2> ${name.common}</h2>
-      <img src = " ${flags.png}">
+      <img src = " ${flags.png}"> <br><br>
+      <button data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-warning" onclick ="singleCountry('${name.common}')"> Explore</button>
     </div>
+    `
+}
+const singleCountry = (countryName) => {
+    fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+        .then(res => res.json())
+        .then(data => displaySingleCountry(data[0]))
+
+}
+const displaySingleCountry = ({ name, area, capital, continents, flags, languages, population, region }) => {
+    // console.log(country);
+    const modalBody = document.getElementById("modalBody")
+    modalBody.innerHTML = `
+    
+    <img src = " ${flags.png}">
+    
+    <p> name : ${name.common}</p>
+    <p> capital : ${capital}</p>
+    <p> area : ${area}</p>
+    <p> population : ${population}</p>
+    <p> region : ${region}</p>
+    <p> continents : ${continents}</p>
+    <p> languages : ${Object.values(languages)}</p>
     `
 }
 loadCountries()
